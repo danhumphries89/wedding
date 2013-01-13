68200b288c7d2715ff3996bca4864e022ed70a7e
Dan Humphries <dan.humphries89@gmail.com>
2013-01-13 19:48:49 +0000

    iPhone size responsiveness added

--- wp-content/themes/Broadway/header.php
+++ wp-content/themes/Broadway/header.php
@@ -6,6 +6,8 @@
 	<title> <?php wp_title('|', true, 'right'); bloginfo( 'name' ); ?> </title>
 	<link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/fonts/fonts.css" rel="stylesheet" type="text/css" screen="all" />
 	<link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/base.css" rel="stylesheet" type="text/css" screen="all" />
+	<link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/responsive.css" rel="stylesheet" type="text/css" screen="all" />
+
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/jquery-1.8.3.min.js" type="text/javascript"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/script.js" type="text/javascript" ></script>
 </head>
