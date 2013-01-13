89f56f89866308734a5070e1c17b51fd18dd8147
Dan Humphries <dan.humphries89@gmail.com>
2013-01-13 14:22:21 +0000

    setup wordpress loop and custom fields to display hotels after the correct page

--- wp-content/themes/Broadway/header.php
+++ wp-content/themes/Broadway/header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
-<html charset="utf-8">
+<html <?php language_attributes(); ?>>
 <head>
-<title></title>
-<link href="/assets/css/base.css" rel="stylesheet" type="text/css" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<title> <?php wp_title('|', true, 'right'); bloginfo( 'name' ); ?> </title>
+	<link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/base.css" rel="stylesheet" type="text/css" screen="all" />
 </head>
 <body>
--- wp-content/themes/Broadway/index.php
+++ wp-content/themes/Broadway/index.php
@@ -1,14 +1,56 @@
 <?php get_header(); ?>
 
-<div id="container" class="wrapper">
+<section class="main_section">
+	<div class="main_items">
+		<?php 
 
-	<div id="main_content">
+			$page_query = new WP_Query( array( 'post_type'=>'page' ));
+			while( $page_query->have_posts() ) : $page_query->the_post(); 
+		?>
+		<div id="page<?php echo get_the_ID(); ?>" class="page">
+			<header class="page_header">
+				<h2><?php the_title(); ?></h2>
+			</header>
+			<section class="page_content">
+				<?php echo the_content(); ?>
+			</section>
+			<section class="page_posts">
+				<?php
+					$page_custom_fields = get_post_meta(get_the_ID());
+					$assigned_posts = $page_custom_fields[assigned_posts][0];
 
+					if($assigned_posts != "") :
+						$post_query = new WP_Query( array( 'post_type'=>'post', 'category_name'=>$assigned_posts, 'orderby'=>'title' ));
+						while( $post_query->have_posts() ) : $post_query->the_post();
+				?>
+					<header class="post_header">
+						<?php $post_custom_fields = get_post_meta( get_the_ID() ); ?>
+						<h3><?php the_title(); ?></h3>
+						<div class="price_block">
+							<span class="price"><?php echo $post_custom_fields[price][0]; ?></span>
+							<span class="sub_price"><?php echo $post_custom_fields[price_sub][0]; ?></span>
+						</div>
+					</header>
+					<section class="post_content">
+						<?php the_content(); ?>
+					</section>
+					<section class="post_address">
+						<?php 
+							$address = explode(",", $post_custom_fields[address][0]);
+							foreach($address as $key=>$address_line) : ?>
+						<span><?php echo trim($address_line); ?></span>
+						<?php endforeach; ?>
+					</section>
+				<?php 
+						wp_reset_query(); 
+						wp_reset_postdata(); 
+						endwhile; 
+					endif;
+				?>
+			</section>
+		</div>
+		<?php endwhile; ?>
 	</div>
-	<div id="left_sidebar">
-		<?php get_sidebar(); ?>
-	</div>
-
-</div>
-
+</section>
+	
 <?php get_footer(); ?>
